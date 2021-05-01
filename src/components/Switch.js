9276cadc35ec14ed436d6591dbe128fbace4acd2
David Wolverton <dwolvert@gmail.com>
2021-05-01 10:48:43 -0400

    Stuff from David

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import "./Switch.css";
 
 function Switch() {
+  const [ on, setOn ] = useState(false);
+
+  let classes;
+  if (on) {
+    classes = "Switch Switch--on";
+  } else {
+    classes = "Switch";
+  }
+
   return (
-    <div className="Switch">
+    <div className={classes}>
       <h3>Switch</h3>
-      <p>OFF</p>
+      <p>{ on ? "ON" : "OFF" }</p>
       <p>
-        <button type="button">On</button>
-        <button type="button">Off</button>
+        <button type="button" onClick={() => setOn(true)}>On</button>
+        <button type="button" onClick={() => setOn(false)}>Off</button>
       </p>
     </div>
   );
