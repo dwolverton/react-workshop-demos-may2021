873960989bcf3b0206f1d49d29ea894d6a90944a
David Wolverton <dwolvert@gmail.com>
2021-05-01 09:44:20 -0400

    first commit

---
+++ src/more-components/Madlibs.js
@@ -0,0 +1,32 @@
+function Madlibs() {
+
+  return (
+    <div className="Madlibs">
+      <form>
+        <p>
+          <label for="Madlibs__adj">Adj: </label>
+          <input
+            id="Madlibs__adj"
+          />
+        </p>
+        <p>
+          <label for="Madlibs__noun">Noun: </label>
+          <input
+            id="Madlibs__noun"
+          />
+        </p>
+        <p>
+          <button type="submit">Show Story</button>
+        </p>
+      </form>
+      <div className="Madlibs__story">
+        <h3>Story</h3>
+        <p>
+          I ordered the ___ fish, but all I got was this outrageous ___!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Madlibs;
