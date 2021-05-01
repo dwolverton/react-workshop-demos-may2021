0c514147afa4a7654280c620b2225e50abe6f4ab
David Wolverton <dwolvert@gmail.com>
2021-05-01 11:49:32 -0400

    Stuff from David

--- src/components/Madlibs.js
+++ src/components/Madlibs.js
@@ -2,12 +2,16 @@ import { useState } from "react";
 
 function Madlibs() {
   const [ adj, setAdj ] = useState("");
+  const [ noun, setNoun ] = useState("");
   // TODO Add a state for the noun as well.
+  const [ isShowStory, setShowStory ] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
     // TODO The story div should initially not show, but submitting
     // the form shows the story.
+    console.log("Do something with ...", adj, noun);
+    setShowStory(true);
   }
 
   return (
@@ -25,18 +29,20 @@ function Madlibs() {
           <label htmlFor="Madlibs__noun">Noun: </label>
           <input
             id="Madlibs__noun"
+            value={noun}
+            onChange={(e) => setNoun(e.target.value)}
           />
         </p>
         <p>
           <button type="submit">Show Story</button>
         </p>
       </form>
-      <div className="Madlibs__story">
+      { isShowStory && <div className="Madlibs__story">
         <h3>Story</h3>
         <p>
-          I ordered the {adj} fish, but all I got was this outrageous ___!
+          I ordered the <strong>{adj}</strong> fish, but all I got was this outrageous <strong>{noun}</strong>!
         </p>
-      </div>
+      </div> }
     </div>
   );
 }
